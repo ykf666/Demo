2cdcc84609ba5beee2262b25998d66d12935b33e
ykf666 <yankefeikobe@163.com>
2017-03-19 22:17:43 +0800

    update

---
+++ src/main/java/com/code/demo/sort/SelectSort.java
@@ -0,0 +1,24 @@
+package com.code.demo.sort;
+
+/**
+ * 选择排序算法 基本思想：在要排序的一组数中，选出最小的一个数与第一个位置的数交换；
+ * 然后在剩下的数当中再找最小的与第二个位置的数交换，如此循环到倒数第二个数和最后一个数比较为止。
+ * 
+ * @author yankefei
+ * @date 2017年3月18日 下午7:54:36
+ *
+ */
+public class SelectSort {
+
+	public static void main(String[] args) {
+		int[] arrayInt = { 14, 60, 40, 22, 15, 99, 67, 51, 32, 15 };
+		for (int i = 0; i < arrayInt.length; i++) {
+			int k = arrayInt[i];
+			for (int j = i + 1; j < arrayInt.length; j++) {
+				int temp = arrayInt[j];
+
+			}
+
+		}
+	}
+}
