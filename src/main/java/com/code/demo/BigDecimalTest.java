06ba6c880369fdfb63962c6afdd23ba644fde4dc
yan.kefei <yan.kefei@opg.cn>
2017-04-05 10:59:16 +0800

    update

---
+++ src/main/java/com/code/demo/BigDecimalTest.java
@@ -0,0 +1,15 @@
+package com.code.demo;
+
+import java.math.BigDecimal;
+
+public class BigDecimalTest {
+	
+	public static void main(String[] args) {
+		
+		BigDecimal bigDecimal = new BigDecimal("499");
+	    BigDecimal bigDecimal2 = new BigDecimal(100);
+	    Float price = bigDecimal.divide(bigDecimal2, 2, BigDecimal.ROUND_HALF_UP).floatValue();
+		System.out.println(price);
+	}
+	
+}
