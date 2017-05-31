6ea97f24d525354b519eb3da115b3088fd1237dc
yan.kefei <yan.kefei@opg.cn>
2017-05-31 17:18:11 +0800

    update

---
+++ src/main/java/com/code/demo/kafka/ConsumerWorker.java
@@ -0,0 +1,21 @@
+package com.code.demo.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsumerWorker implements Runnable {
+
+	private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerExample.class);
+
+	private ConsumerRecord<String, String> record;
+
+	public ConsumerWorker(ConsumerRecord<String, String> record) {
+		this.record = record;
+	}
+
+	public void run() {
+		logger.info("thread = {}, partition = {}, offset = {}, key = {}, value = {}", Thread.currentThread().getId(),
+				record.partition(), record.offset(), record.key(), record.value());
+	}
+}
---
+++ src/main/java/com/code/demo/kafka/KafkaMain.java
@@ -0,0 +1,19 @@
+package com.code.demo.kafka;
+
+public class KafkaMain {
+	public static void main(String[] args) {
+		String brokerList = "10.60.1.246:9092,10.60.1.248:9092,10.60.1.249:9092";
+		String groupId = "consumer-group3";
+		String topic = "topic3";
+		int workerNum = 5;
+
+		ConsumerHandler consumers = new ConsumerHandler(workerNum, brokerList, groupId, topic);
+		consumers.execute(workerNum);
+		try {
+			Thread.sleep(1000000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		consumers.shutdown();
+	}
+}
