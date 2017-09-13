062534726fa59abecc7278025fa39314370f2213
yan.kefei <yan.kefei@opg.cn>
2017-09-13 17:16:12 +0800

    atomicInteger

---
+++ src/main/java/com/code/demo/simple/AtomicIntegerTest.java
@@ -0,0 +1,35 @@
+package com.code.demo.simple;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerTest {
+
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    private static class AddThread implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                count.getAndIncrement();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService threadpool = Executors.newFixedThreadPool(10);
+
+        for (int k = 0; k < 10; k++) {
+            threadpool.submit(new AddThread());
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("result of acumulated sum =" + count);
+        threadpool.shutdown();
+    }
+}
