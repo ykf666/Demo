6a196516712327479deed1f633e54f5ea08c5066
yan.kefei <yan.kefei@opg.cn>
2017-09-15 18:16:41 +0800

    排序算法

---
+++ src/main/java/com/code/demo/sort/BubbleSort.java
@@ -0,0 +1,46 @@
+package com.code.demo.sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序算法
+ *
+ * 基本思想：相邻两元素比较，前面比后面大，则交换位置，一趟之后，最小的就会交换的第一个位置
+ * @author yan.kefei
+ * @date 2017/9/15 17:22
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr1 = {6, 2, -5, 10, 104, 12, 92, 87};
+        int[] arr2 = {6, 2, -5, 10, 104, 12, 92, 87};
+        sort1(arr1);
+        System.out.println();
+        sort2(arr2);
+    }
+
+    private static void sort1(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    private static void sort2(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr.length-1; j > i; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
--- src/main/java/com/code/demo/sort/QuickSort.java
+++ src/main/java/com/code/demo/sort/QuickSort.java
@@ -2,10 +2,26 @@ package com.code.demo.sort;
 
 import java.util.Arrays;
 
+/**
+ * 快速排序算法
+ *
+ * 基本思想：
+ * 1、以一个数为基准数，一般为第一个数
+ * 2、分治法，把数组中比基准大的放右边，比基准小的放左边
+ * 3、递归排序子数组
+ *
+ * 具体实现：
+ * 1、从右边开始遍历，直到比基准小的数停止，然后从左边遍历，直到比基准大的数停止，如果此时数组仍没有遍历结束，则先交换两个循环遍历结束位置的值
+ * 2、继续遍历，直到数组遍历完成，即两边遍历的位置交叉，把基准数放置到此处，此处左边都是比基准数小的值，右边都比它大
+ * 3、此时，数组被划分为两个子数组，则递归子数组排序
+ *
+ * @author yan.kefei
+ * @date 2017年9月14日 下午10:45:02
+ */
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] arr = {2, -5, 33, 10, 4, 102, 92, 87};
+        int[] arr = {6, 2, -5, 10, 104, 12, 92, 87};
         quick_sort(arr, 0, arr.length - 1);
         System.out.print(Arrays.toString(arr));
     }
@@ -36,8 +52,8 @@ public class QuickSort {
                 arr[i] = arr[j];
                 arr[j] = tt;
             }
-
         }
+        System.out.println(Arrays.toString(arr));
 
         //交换基位数据
         int kk = arr[i];
