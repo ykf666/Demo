adb34f5dd81f481747edead928fe2225b8151eab
yan.kefei <yan.kefei@opg.cn>
2017-11-01 14:05:58 +0800

    multiThreadDemo

---
+++ src/main/java/com/code/demo/MultiThread/SyncThread.java
@@ -0,0 +1,40 @@
+package com.code.demo.MultiThread;
+
+/**
+ * @author yan.kefei
+ * @date 2017/11/1 13:51
+ */
+public class SyncThread implements Runnable {
+    private static int count;
+
+    public SyncThread() {
+        count = 0;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + ":" + (count++));
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SyncThread syncThread = new SyncThread();
+        Thread thread1 = new Thread(syncThread, "SyncThread1");
+        Thread thread2 = new Thread(syncThread, "SyncThread2");
+//        Thread thread1 = new Thread(new SyncThread(),"SyncThread1");
+//        Thread thread2 = new Thread(new SyncThread(),"SyncThread2");
+        thread1.start();
+        thread2.start();
+
+
+    }
+
+}
