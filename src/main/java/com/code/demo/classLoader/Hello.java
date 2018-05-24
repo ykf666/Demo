6ec5dab893c078f0752c7eee2df357ac62d6c7d2
ykf666 <yankefeikobe@163.com>
2018-05-24 23:05:25 +0800

    custom ClassLoader

---
+++ src/main/java/com/code/demo/classLoader/Hello.java
@@ -0,0 +1,12 @@
+package com.code.demo.classLoader;
+
+/**
+ * @author yan.kefei
+ * @date 2018/5/24 22:49
+ */
+public class Hello {
+
+    public void say() {
+        System.out.println("say hello");
+    }
+}
