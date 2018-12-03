4cbf5b1cc1c1b468572d9a27a53a1ad47593f474
yankefei <2500627332@qq.com>
2018-12-04 00:23:14 +0800

    update

--- src/main/java/com/code/demo/MultiThread/FutureDemo.java
+++ src/main/java/com/code/demo/MultiThread/FutureDemo.java
@@ -1,5 +1,8 @@
 package com.code.demo.MultiThread;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.*;
 
 /**
@@ -9,35 +12,65 @@ public class FutureDemo {
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        Task task = new Task();
+        Task task1 = new Task(3);
+        Task task2 = new Task(5);
         //Future和FutureTask的区别
         //Futrue是只能结合ExecutorService线程池使用，new Thread是不支持Future构造的
-        Future<Integer> future = executorService.submit(task);
-        executorService.shutdown();
+        List<Future<Integer>> futures = new ArrayList<>();
+        futures.add(executorService.submit(task1));
+        futures.add(executorService.submit(task2));
 
-        int result = 0;
+        //如果有多个任务提交执行，返回结果也将按照提交顺序获得
+        List<Integer> results = new ArrayList<>();
         try {
-            result = future.get();
+            Iterator iterator = futures.iterator();
+            //方式一：阻塞主线程获取结果
+/*            while (iterator.hasNext()){
+                Future<Integer> future = (Future<Integer>) iterator.next();
+                int result = future.get();
+                results.add(result);
+            }*/
+
+            //方式二：轮询结果
+            while (iterator.hasNext()) {
+                Future<Integer> future = (Future<Integer>) iterator.next();
+                if (future.isDone() && !future.isCancelled()) {
+                    int result = future.get();
+                    results.add(result);
+                    System.out.println("轮询结果计算完成，get...");
+                    iterator.remove();
+                } else {
+                    Thread.sleep(1000);
+                    System.out.println("未得到结果，等待...");
+                }
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        System.out.println("运行结果：" + result);
+        System.out.println("运行结果：" + results);
     }
 
 }
 
 class Task implements Callable<Integer> {
 
+    private final int num;
+
+    public Task(int num) {
+        this.num = num;
+    }
+
     @Override
     public Integer call() throws Exception {
         System.out.println(Thread.currentThread().getName() + " doing something...");
         int sum = 0;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < num; i++) {
             sum += i;
             Thread.sleep(1000);
         }
+        System.out.println(Thread.currentThread().getName() + " 计算结果为：" + sum);
         return sum;
     }
 }
--- src/main/java/com/code/demo/MultiThread/FutureTaskDemo.java
+++ src/main/java/com/code/demo/MultiThread/FutureTaskDemo.java
@@ -5,22 +5,42 @@ import java.util.concurrent.FutureTask;
 
 /**
  * Created by yankefei on 2018/12/3.
+ * <p>
+ * futureTask弥补了future只能用线程池提交的不足，可以使用Thread提交
+ * get阻塞获取结果前，可以穿插其他业务逻辑
  */
 public class FutureTaskDemo {
 
     public static void main(String[] args) {
-        FutureTask<Integer> futureTask = new FutureTask<>(new Task());
+        long t1 = System.currentTimeMillis();
+        FutureTask<Integer> futureTask = new FutureTask<>(new Task(5));
         Thread thread = new Thread(futureTask);
         thread.start();
 
+        //线程启动后，主线程可执行其他逻辑
+        boolean cancle = false;
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         int result = 0;
         try {
-            result = futureTask.get();
+            System.out.println("主线程穿插业务执行完成！");
+            if (cancle) {
+                System.out.println(thread.getName() + "取消了...");
+                futureTask.cancel(true);
+            } else {
+                result = futureTask.get();
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+        long t3 = System.currentTimeMillis();
+        System.out.println("主线程共耗时：" + (t3 - t1));
         System.out.println("futureTask计算结果为：" + result);
     }
 }
