c4437a0e3f98dde1744749be08607e7fdd12fe67
闫克非_集团 <yankefei@longfor.com>
2020-12-02 19:48:43 +0800

    update

---
+++ src/main/java/com/code/leetCode/SortIntBybit.java
@@ -0,0 +1,43 @@
+package com.code.leetCode;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+
+/**
+ * Created by yankefei on 2020/12/2.
+ * 根据整数二进制中包含1的个数排序，如果个数相同，按照大小排序
+ */
+public class SortIntBybit {
+
+    public static void main(String[] args) {
+        int[] array = {8, 11, 17, 190, 32, 226};
+        List<Integer> list = new ArrayList<>();
+        for (int i : array) {
+            list.add(i);
+            System.out.println(statisticsCountOne(i) + "   " + getBits(i));
+        }
+        Collections.sort(list, (o1, o2) -> {
+            int c = statisticsCountOne(o1) - statisticsCountOne(o2);
+            if (c == 0) {
+                return o1 - o2;
+            } else {
+                return c;
+            }
+        });
+        System.out.println(StringUtils.join(list, " ,"));
+    }
+
+    private static String getBits(int value) {
+        return Integer.toBinaryString(value);
+    }
+
+    private static int statisticsCountOne(int value) {
+        int count = 0;
+        while (value != 0) {
+            count += value % 2;
+            value /= 2;
+        }
+        return count;
+    }
+}
