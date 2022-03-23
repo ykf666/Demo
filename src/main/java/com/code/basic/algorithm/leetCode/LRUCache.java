aa99cc9a166624142b4ee369ae0e76e48e693597
ykf666 <2500627332@qq.com>
2022-03-24 00:05:02 +0800

    lru

--- src/main/java/com/code/basic/algorithm/leetCode/LRUCache.java
+++ src/main/java/com/code/basic/algorithm/leetCode/LRUCache.java
@@ -3,47 +3,49 @@ package com.code.basic.algorithm.leetCode;
 import java.util.*;
 
 /**
+ * case 146
  * Least recently used（最近最少使用）
+ * linkedHashMap实现
  * Created by yankefei on 2022/3/21.
  */
-public class Case146_LRUCache {
+public class LRUCache {
 
-    private LinkedHashMap<Integer, Integer> linkedHashMap;
+    private LinkedHashMap<Integer, Integer> map;
 
     private int capacity;
 
-    public Case146_LRUCache(int capacity) {
+    public LRUCache(int capacity) {
         this.capacity = capacity;
-        linkedHashMap = new LinkedHashMap(capacity);
+        this.map = new LinkedHashMap<>(capacity);
     }
 
     public Integer get(int key) {
         Integer value = null;
-        if (this.linkedHashMap.containsKey(key)) {
-            value = this.linkedHashMap.get(key);
-            this.linkedHashMap.remove(key);
-            this.linkedHashMap.put(key, value);
+        if (this.map.containsKey(key)) {
+            value = this.map.get(key);
+            this.map.remove(key);
+            this.map.put(key, value);
         }
         return value;
     }
 
     public void put(Integer key, Integer value) {
-        if (this.linkedHashMap.containsKey(key)) {
-            this.linkedHashMap.remove(key);
-        } else if (this.linkedHashMap.size() == this.capacity) {
+        if (this.map.containsKey(key)) {
+            this.map.remove(key);
+        } else if (this.map.size() == this.capacity) {
             //删除最早的缓存
-            this.linkedHashMap.remove(this.linkedHashMap.entrySet().iterator().next().getKey());
+            this.map.remove(this.map.entrySet().iterator().next().getKey());
         }
-        this.linkedHashMap.put(key, value);
+        this.map.put(key, value);
     }
 
     public void show() {
-        Set<Map.Entry<Integer, Integer>> entrySet = this.linkedHashMap.entrySet();
+        Set<Map.Entry<Integer, Integer>> entrySet = this.map.entrySet();
         System.out.println(Arrays.toString(entrySet.toArray()));
     }
 
     public static void main(String[] args) {
-        Case146_LRUCache lruCache = new Case146_LRUCache(2);
+        LRUCache lruCache = new LRUCache(2);
         lruCache.put(1, 1);
         lruCache.put(2, 2);
         lruCache.show();
